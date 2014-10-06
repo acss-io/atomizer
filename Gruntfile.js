189d74c7626e7d2d4abc11b95a75f1c75faad45e
renatoi <renatoi@yahoo-inc.com>
2014-10-06 10:09:08 -0700

    Initial set up of the repo.

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Configurable paths for the application
+    var appConfig = {
+        src: 'src',
+        docs: 'docs',
+        dist: 'dist',
+        pkg: grunt.file.readJSON('package.json')
+    };
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+
+        // -- Project Config -------------------------------------------------------
+        project: appConfig,
+
+        // -- SASS Config -------------------------------------------------------
+        sass: {
+            options: {
+                sourcemap: 'none',
+                style: 'expanded',
+                banner: [
+                    '/*!',
+                    'Atomic.css v<%= project.pkg.version %>',
+                    'Copyright 2014 Yahoo! Inc. All rights reserved.',
+                    'Licensed under the BSD License.',
+                    'https://github.com/yahoo/atomic.css/blob/master/LICENSE.md',
+                    '*/\n'
+                ].join('\n')
+            },
+            atomic: {
+                files: [{
+                    expand: true,
+                    cwd: '<%=project.src %>',
+                    src: ['**/*.scss'],
+                    dest: './dist',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        // -- SCSS Lint Config -------------------------------------------------------
+        scsslint: {
+            all: [
+                '<%= project.src %>/**/*.scss'
+            ],
+            options: {
+                config: '.scss-lint.yml',
+                reporterOutput: 'scss-lint-report.xml',
+                colorizeOutput: true
+            }
+        },
+
+        // -- SCSS Lint Config -------------------------------------------------------
+        clean: {
+            dist: {
+                files: [{
+                    src: '<%=project.dist%>',
+                    dot: true
+                }]
+            }
+        },
+
+        // -- Shell Config -------------------------------------------------------
+        shell: {
+            bower: {
+                command: 'bower install'
+            }
+        }
+    });
+
+    grunt.registerTask('default', [
+        'clean',
+        'lint',
+        'sass'
+    ]);
+
+    // publish src to github page
+    grunt.registerTask('publish', [
+        'gh-pages'
+    ]);
+
+    // publish src to github page
+    grunt.registerTask('lint', [
+        'scsslint'
+    ]);
+};
