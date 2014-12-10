8430ba3dccf5ac4668454da4b841f5d480397868
renatoi <renatoi@yahoo-inc.com>
2014-12-10 15:05:15 -0800

    Create atomicObjects to keep it in a separate file

--- src/absurd/atomic.js
+++ src/absurd/atomic.js
@@ -109,53 +109,8 @@
 var _ = require('lodash');
 
 var AtomicBuilder = require('./AtomicBuilder.js');
-var atomicConfig = require('./AtomicConfig.js');
-var atomicObjs = [
-    {
-        type: 'pattern',
-        id: 'font-weight',
-        name: 'Font weight',
-        prefix: '.Fw-',
-        properties: ['font-weight'],
-        rules: [
-            {suffix: 'n', values: ['normal']},
-            {suffix: 'b', values: ['bold']},
-            {suffix: 'br', values: ['bolder']},
-            {suffix: 'lr', values: ['lighter']},
-            {suffix: '100', values: ['100']},
-            {suffix: '200', values: ['200']},
-            {suffix: '300', values: ['300']},
-            {suffix: '400', values: ['400']},
-            {suffix: '500', values: ['500']},
-            {suffix: '600', values: ['600']},
-            {suffix: '700', values: ['700']},
-            {suffix: '800', values: ['800']},
-            {suffix: '900', values: ['900']}
-        ]
-    },
-    {
-        type: 'pattern',
-        id: 'padding-x',
-        name: 'Horizontal padding',
-        prefix: '.Px-',
-        allowCustom: true,
-        properties: ['padding-left', 'padding-right'],
-        rules: [
-            {suffix: 'a', values: ['auto', 'auto']}
-        ]
-    },
-    {
-        type: 'rule',
-        id: 'bfc',
-        name: 'Block formatting context',
-        rule: {
-            '.Bfc': {
-                'overflow': 'hidden',
-                '*zoom': 1
-            }
-        }
-    }
-];
+var atomicConfig = require('./atomicConfig.js');
+var atomicObjs = require('./atomicObjects.js');
 
 module.exports = function(api) {
 
