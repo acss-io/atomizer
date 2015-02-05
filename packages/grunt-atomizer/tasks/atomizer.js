e7d50a39b8caaba8fc7ec5cf08929dfda8510c10
Steve Carlson <yasteve@yahoo-inc.com>
2015-02-05 15:38:40 -0800

    Creating grunt task for Atomizer

---
+++ packages/grunt-atomizer/tasks/atomizer.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Atomizer = require('atomizer');
+
+module.exports = function (grunt) {
+
+    grunt.registerMultiTask('atomizer', 'Grunt plugin to execute Atomizer', function () {
+        var done = this.async();
+        var options = this.options();
+
+        if (options.require && options.require.length > 0) {
+            options.require = grunt.file.expand(options.require);
+        }
+
+        this.files.forEach(function (f) {
+            Atomizer(f.src, f.rules, options, f.dest, done);
+        });
+
+    });
+};
