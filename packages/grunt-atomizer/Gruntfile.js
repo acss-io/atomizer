bba4fda89426ae848b8650283dee2f54f7d5a3ee
Steve Carlson <src-code@users.noreply.github.com>
2015-02-12 16:06:28 -0800

    Merge pull request #3 from yahoo/test-fix
    Attempt to fix tests.

---
+++ packages/grunt-atomizer/Gruntfile.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2015, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                'test/'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        },
+
+        // Configuration to be run (and then tested).
+        atomizer: {
+            default_options: {
+                options: {},
+                files: {
+                    'tmp/default-options.css': ['test/fixtures/default-config.js']
+                }
+            }
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
+        }
+
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'atomizer', 'nodeunit']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
+
+};
--- packages/grunt-atomizer/tasks/atomizer.js
+++ packages/grunt-atomizer/tasks/atomizer.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2015, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+
 'use strict';
 
 var atomizer = require('atomizer');
