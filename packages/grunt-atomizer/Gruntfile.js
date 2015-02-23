3144248a508971784b2ac28aa1f455ee14384c95
Renato Iwashima <renatoiwa@gmail.com>
2015-02-23 11:48:41 -0800

    Merge pull request #4 from yahoo/autoatom
    Autoatom

--- packages/grunt-atomizer/Gruntfile.js
+++ packages/grunt-atomizer/Gruntfile.js
@@ -27,11 +27,85 @@ module.exports = function(grunt) {
 
         // Configuration to be run (and then tested).
         atomizer: {
-            default_options: {
-                options: {},
-                files: {
-                    'tmp/default-options.css': ['test/fixtures/default-config.js']
-                }
+            // here we test if passing a configFile works as expected
+            configFileOnly: {
+                options: {
+                    configFile: 'test/fixtures/sample-config.js'
+                },
+                files: [
+                    {
+                        dest: 'tmp/configFileOnly.css'
+                    }
+                ]
+            },
+            // here we test if passing config to grunt task directly works as expected
+            configGruntOnly: {
+                options: {
+                    config: {
+                        config: {
+                            namespace: '#atomic',
+                            start: 'left',
+                            end: 'right'
+                        },
+                        display: {
+                            ib: true
+                        }
+                    }
+                },
+                files: [
+                    {
+                        dest: 'tmp/configGruntOnly.css'
+                    }
+                ]
+            },
+            // here we test which one has precedence (config declared in grunt task directly first then configFile second)
+            configBoth: {
+                options: {
+                    configFile: 'test/fixtures/sample-config.js',
+                    config: {
+                        config: {
+                            namespace: '#atomic',
+                            start: 'left',
+                            end: 'right'
+                        },
+                        'border-top': {
+                            custom: [
+                                {suffix: '1', values: ['10px solid #ccc']}
+                            ]
+                        },
+                        display: {
+                            ib: true
+                        }
+                    }
+                },
+                files: [
+                    {
+                        dest: 'tmp/configBoth.css'
+                    }
+                ]
+            },
+            // here we test both cases and also parsing the HTML files
+            // Grunt task should warn about 'Bdb-1' missing in config (found in parsing)
+            configBothWithParsing: {
+                options: {
+                    configFile: 'test/fixtures/sample-config.js',
+                    config: {
+                        config: {
+                            namespace: '#atomic',
+                            start: 'left',
+                            end: 'right'
+                        },
+                        display: {
+                            b: true
+                        }
+                    }
+                },
+                files: [
+                    {
+                        src: ['test/fixtures/*.html'],
+                        dest: 'tmp/configBothWithParsing.css'
+                    }
+                ]
             }
         },
 
--- packages/grunt-atomizer/test/fixtures/sample-config.js
+++ packages/grunt-atomizer/test/fixtures/sample-config.js
@@ -13,5 +13,12 @@ module.exports = {
     // pattern
     'display': {
         'b': true
+    },
+
+    // pattern
+    'border-top': {
+        custom: [
+            {suffix: '1', values: ['1px solid #ccc']}
+        ]
     }
 };
