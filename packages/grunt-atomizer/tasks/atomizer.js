29d2b1c3a3de422eb3cab61af4bb0fbd41a24caa
Renato Iwashima <renatoiwa@gmail.com>
2015-02-23 17:35:27 -0800

    Merge pull request #7 from yahoo/find-custom-suffix
    Find custom suffix

--- packages/grunt-atomizer/tasks/atomizer.js
+++ packages/grunt-atomizer/tasks/atomizer.js
@@ -114,15 +114,17 @@ module.exports = function (grunt) {
     function handleCustomConfigRule(configRule, className, pattern, suffix, currentConfig) {
         var value;
 
-        if (pattern.allowSuffixToValue) {
+        if (pattern.allowSuffixToValue && (
+            isPercentage(suffix) || 
+            isLength(suffix) || 
+            isHex(suffix) || 
+            isInteger(suffix) || 
+            isFloat(suffix))) {
+
             if (!configRule[pattern.id].custom) {
                 configRule[pattern.id].custom = [];
             }
 
-            if (!isPercentage(suffix) && !isLength(suffix) && !isHex(suffix) && !isInteger(suffix) && !isFloat(suffix)) {
-                grunt.log.warn('Class ' + className + ' cannot be automatically generated because it doesn\'t have a valid value: ' + suffix);
-                return false;
-            }
             value = isHex(suffix) ? '#' + suffix : suffix;
             configRule[pattern.id].custom.push({
                 suffix: suffix,
@@ -275,7 +277,7 @@ module.exports = function (grunt) {
                 // custom-only patterns with no rules
             }
             if (pattern.prefix) {
-                regexes.push(escapeRegExp(prefix) + '(?:(?:neg)?[0-9]+(?:\.[0-9]+)?(?:[a-zA-Z%]+)?|[0-9a-f]{3}(?:[0-9a-f]{3})?)');
+                regexes.push(escapeRegExp(prefix) + '(?:(?:neg)?[0-9]+(?:\.[0-9]+)?(?:[a-zA-Z%]+)?|[0-9a-f]{3}(?:[0-9a-f]{3})?|[a-zA-Z0-9%\.]+\\b)');
             }
         });
         return new RegExp('(' + regexes.join('|') + ')', 'g');
