887d35cf810298b67376fe3c132e6aaf79913b26
Steve Carlson <yasteve@yahoo-inc.com>
2015-03-25 16:32:43 -0700

    Bringing back mergeConfigs()

--- tests/utils.js
+++ tests/utils.js
@@ -21,4 +21,102 @@ describe('utils', function () {
             expect(result).to.deep.equal(expected);
         });
     });
+
+    describe('mergeConfigs()', function () {
+        it('should merge non-conflicting breakpoints correctly', function () {
+            var config1 = {
+                breakPoints: {
+                    'sm': '@media(min-width: 600px)'
+                }
+            };
+            var config2 = {
+                breakPoints: {
+                    'md': '@media(min-width: 800px)'
+                }
+            };
+            var expected = {
+                breakPoints: {
+                    'sm': '@media(min-width: 600px)',
+                    'md': '@media(min-width: 800px)'
+                }
+            };
+            var result = utils.mergeConfigs([config1, config2]);
+            expect(result).to.deep.equal(expected);
+        });
+        it('should merge conflicting breakpoints where the latter config is kept', function () {
+            var config1 = {
+                breakPoints: {
+                    'sm': '@media(min-width: 600px)'
+                }
+            };
+            var config2 = {
+                breakPoints: {
+                    'sm': '@media(min-width: 550px)'
+                }
+            };
+            var expected = {
+                breakPoints: {
+                    'sm': '@media(min-width: 550px)'
+                }
+            };
+            var result = utils.mergeConfigs([config1, config2]);
+            expect(result).to.deep.equal(expected);
+        });
+        it('should merge classNames correctly', function () {
+            var config1 = {
+                classNames: ['D-ib', 'Bd-foo']
+            };
+            var config2 = {
+                classNames: ['D-n!', 'D-ib']
+            };
+            var config3 = {
+                classNames: ['C-#333', 'D-ib']
+            };
+            var expected = {
+                classNames: ['D-ib', 'Bd-foo', 'D-n!', 'C-#333']
+            };
+            var result = utils.mergeConfigs([config1, config2, config3]);
+
+            expect(result).to.deep.equal(expected);
+        });
+        it('should merge customs correctly', function () {
+            var config1 = {
+                custom: {
+                    'foo': '1px solid red'
+                }
+            };
+            var config2 = {
+                custom: {
+                    'bar': '2px dashed blue'
+                }
+            };
+            var expected = {
+                custom: {
+                    'foo': '1px solid red',
+                    'bar': '2px dashed blue'
+                }
+            };
+            var result = utils.mergeConfigs([config1, config2]);
+            expect(result).to.deep.equal(expected);
+        });
+        it('should merge conflicting customs where the latter config is kept', function () {
+            var config1 = {
+                custom: {
+                    'foo': '1px solid red'
+                }
+            };
+            var config2 = {
+                custom: {
+                    'foo': '2px dashed blue'
+                }
+            };
+            var expected = {
+                custom: {
+                    'foo': '2px dashed blue'
+                }
+            };
+            var result = utils.mergeConfigs([config1, config2]);
+            expect(result).to.deep.equal(expected);
+        });
+    });
 });
