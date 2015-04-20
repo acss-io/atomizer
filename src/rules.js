53c45c781b79bb40e2fabd16043f29e436e29970
renatoi <renatoi@yahoo-inc.com>
2015-04-20 11:02:50 -0700

    Adding comments back

--- src/rules.js
+++ src/rules.js
@@ -19,7 +19,7 @@
  * NOTES:
  *
  * Depending on the selector you use to namespace these rules (id versus class),
- * their style weight will be either 0.1.1.0 or 0.0.2.0. We suggest using an id
+ * their style weight will be either 0,1,1,0 or 0,0,2,0. We suggest using an id
  * for the extra specificity.
  *
  * - look for top/right/bottom/left rules in the "offset" section.
