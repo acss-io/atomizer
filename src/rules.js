10b83b4d1d41d450e21d1d4434a7aa84df81ba08
Roderick Hsiao <roderickhsiao@gmail.com>
2015-05-07 16:06:17 -0700

    Update allowParamToValue

--- src/rules.js
+++ src/rules.js
@@ -2260,9 +2260,9 @@ module.exports = [
         "type": "pattern",
         "name": "Matrix (transform)",
         "matcher": "Matrix",
-        "allowParamToValue": true,
+        "allowParamToValue": false,
         "styles": {
-            "transform": "matrix($0,$1,$2,$3,$4,$5)"
+            "transform": "matrix($0)"
         }
     },
     // matrix3d
@@ -2272,7 +2272,7 @@ module.exports = [
         "matcher": "Matrix3d",
         "allowParamToValue": false,
         "styles": {
-            "transform": "matrix($0,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15)"
+            "transform": "matrix($0)"
         }
     },
     // rotate
