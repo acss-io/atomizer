f2eea2ed8d4d62227904beb84af904ce94d41548
renatoi <renatoi@yahoo-inc.com>
2015-05-21 17:14:20 -0700

    added support for color keywords

--- tests/atomizer.js
+++ tests/atomizer.js
@@ -281,7 +281,7 @@ describe('Atomizer()', function () {
         it ('returns css by reading an array of class names', function () {
             var atomizer = new Atomizer();
             var config = {
-                classNames: ['Px(inh)', 'Trsdu(.3s)', 'sibling:c+D(n)', 'End(0)', 'Ta(start)', 'Ta(end)', 'Bgc(#fff.4)', 'Bgc(#fff)', 'P(55px)', 'H(100%)', 'M(a)', 'test:h>Op(1):h', 'test:h_Op(1):h', 'Op(1)', 'Op(1)!', 'D(n)!', 'C(#333)', 'C(#333):li', 'Mt(-10px)', 'W(1/3)', 'Bgz(45px)']
+                classNames: ['C(red)', 'Px(inh)', 'Trsdu(.3s)', 'sibling:c+D(n)', 'End(0)', 'Ta(start)', 'Ta(end)', 'Bgc(#fff.4)', 'Bgc(#fff)', 'P(55px)', 'H(100%)', 'M(a)', 'test:h>Op(1):h', 'test:h_Op(1):h', 'Op(1)', 'Op(1)!', 'D(n)!', 'C(#333)', 'C(#333):li', 'Mt(-10px)', 'W(1/3)', 'Bgz(45px)']
             };
             var expected = [
                 '.Bgc\\(\\#fff\\.4\\) {',
@@ -292,6 +292,9 @@ describe('Atomizer()', function () {
                 '}',
                 '.Bgz\\(45px\\) {',
                 '  background-size: 45px;',
+                '}',
+                '.C\\(red\\) {',
+                '  color: red;',
                 '}',
                 '.C\\(\\#333\\), .C\\(\\#333\\)\\:li:link {',
                 '  color: #333;',
