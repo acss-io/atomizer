fad58dc395870526e1bf1e486ed043c25f5243c0
Steve Carlson <src-code@users.noreply.github.com>
2016-02-22 16:05:08 -0800

    Merge pull request #105 from yahoo/atomizer-web
    Removing aria labels

--- packages/acss-site/app/components/PageReference.jsx
+++ packages/acss-site/app/components/PageReference.jsx
@@ -15,11 +15,8 @@ class PageReference extends React.Component {
             <div id="reference" role="main" className="reference-page innerwrapper Mb(50px) Mx(10px) Maw(1000px)--sm Mx(a)--sm W(90%)--sm">
                 <h1>Reference</h1>
                 <p>Use this page to search for Atomic classes.</p> 
-                <p>You can also <a href="/guides/syntax.html" aria-label="about the syntax">learn about the syntax</a>, 
-                   <a href="/guides/syntax.html#examples-" aria-label="examples">look at some examples</a>, 
-                   <a href="/guides/atomic-classes.html" aria-label="about custom classes">learn about custom classes</a>, 
-                   or <a href="/guides/helper-classes.html" aria-label="about the helpers">learn about helper/utility classes</a>.  
-                   There is also <a href="/guides/atomizer.html#web-tools" aria-label="web tools">a handy web tool</a> to help you experiment with Atomizer syntax.
+                <p>You can also <a href="/guides/syntax.html">learn about the syntax</a>, <a href="/guides/syntax.html#examples-">look at some examples</a>, <a href="/guides/atomic-classes.html">learn about custom classes</a>, or <a href="/guides/helper-classes.html">learn about helper/utility classes</a>.  
+                   There is also <a href="/guides/atomizer.html#web-tools">a handy web tool</a> to help you experiment with Atomizer syntax.
                 </p>
                 <Reference />
             </div>
