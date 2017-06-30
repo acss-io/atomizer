17f3908f087c6f9c0a13e25186db45d5e820aa4f
Steve Carlson <src-code@users.noreply.github.com>
2017-06-30 15:07:45 -0700

    Merge pull request #122 from acss-io/helpers-reference
    Adding Helpers to the reference page

--- packages/acss-site/app/components/PageReference.jsx
+++ packages/acss-site/app/components/PageReference.jsx
@@ -14,7 +14,7 @@ class PageReference extends React.Component {
         return (
             <div id="reference" role="main" className="reference-page innerwrapper Mb(50px) Mx(10px) Maw(1000px)--sm Mx(a)--sm W(90%)--sm">
                 <h1>Reference</h1>
-                <p>Use this page to search for Atomic classes.</p> 
+                <p>Use this page to search for <a href="#atomic-classes">Atomic</a> and <a href="#helper-classes">Helper</a> classes.</p> 
                 <p>You can also <a href="/guides/syntax.html">learn about the syntax</a>, <a href="/guides/syntax.html#examples-">look at some examples</a>, <a href="/guides/atomic-classes.html">learn about custom classes</a>, or <a href="/guides/helper-classes.html">learn about helper/utility classes</a>.  
                    There is also <a href="/guides/atomizer.html#web-tools">a handy web tool</a> to help you experiment with Atomizer syntax.
                 </p>
