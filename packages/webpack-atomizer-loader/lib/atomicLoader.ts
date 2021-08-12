5da3b7d07e58db53a400fc8a0546812e762e2c50
Sebastian Landwehr <info@sebastianlandwehr.com>
2021-08-12 18:01:00 +0200

    fix: do not require rules but instead pass them directly as objects (#36)

--- packages/webpack-atomizer-loader/lib/atomicLoader.ts
+++ packages/webpack-atomizer-loader/lib/atomicLoader.ts
@@ -52,20 +52,22 @@ const parseAndGenerateFile = function (
         }
 
         const pathConfig: PathConfig = configObject[configPath] || configObject.default;
+
+        // custom rules file
+        if (pathConfig.options && pathConfig.options.rules) {
+            atomizer.addRules(
+                typeof pathConfig.options.rules === 'string'
+                    ? require(require.resolve(pathConfig.options.rules))
+                    : pathConfig.options.rules
+            );
+        }
+
         const foundClasses = atomizer.findClassNames(source);
         let cssDest = pathConfig.cssDest || DEFAULT_CSS_DEST;
 
         if (!ensureExists(cssDest)) {
             console.warn('[atomic loader] create css failed.');
             return;
-        }
-
-        // custom rules file
-        if (pathConfig.options && pathConfig.options.rules) {
-            const customRules = require(require.resolve(pathConfig.options.rules));
-            if (customRules) {
-                atomizer.addRules(customRules);
-            }
         }
 
         const finalConfig = atomizer.getConfig(foundClasses, pathConfig.configs || {});
--- packages/webpack-atomizer-loader/tests/atomicLoader.js
+++ packages/webpack-atomizer-loader/tests/atomicLoader.js
@@ -2,6 +2,7 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 var atomicLoader = require('../dist/atomicLoader');
+var path = require('path');
 
 describe('atomic loader', () => {
     it('can generate correct css', (done) => {
@@ -12,5 +13,29 @@ describe('atomic loader', () => {
             done();
         };
         atomicLoader.call(this, '<div class="Bgc(yellow)"></div>');
+    });
+    it('rules path', (done) => {
+        this.async = () => (err, source) => {
+            var cssReg = new RegExp(/\.Foo/);
+            const cssFile = fs.readFileSync('./build/css/atomic.css', 'utf8');
+            expect(cssReg.test(cssFile)).to.equal(true);
+            done();
+        };
+        this.query = {
+            configPath: path.resolve(__dirname, 'fixtures', 'rules-path.config.js'),
+        };
+        atomicLoader.call(this, '<div class="Foo"></div>');
+    });
+    it('rules object', (done) => {
+        this.async = () => (err, source) => {
+            var cssReg = new RegExp(/\.Foo/);
+            const cssFile = fs.readFileSync('./build/css/atomic.css', 'utf8');
+            expect(cssReg.test(cssFile)).to.equal(true);
+            done();
+        };
+        this.query = {
+            configPath: path.resolve(__dirname, 'fixtures', 'rules-object.config.js'),
+        };
+        atomicLoader.call(this, '<div class="Foo"></div>');
     });
 });
