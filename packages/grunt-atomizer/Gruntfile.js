bc02637f967c443712eed51ff34d2eafb989a146
redonkulus <seth@bertalotto.net>
2022-07-15 09:21:13 -0700

    chore: clean up files

--- packages/grunt-atomizer/Gruntfile.js
+++ packages/grunt-atomizer/Gruntfile.js
@@ -3,23 +3,13 @@
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 
-'use strict';
-
 module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt, {
+        config: '../../package.json',
+        requireResolution: true
+    });
 
-    // Project configuration.
     grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js',
-                'test/'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-
         // Before generating any new files, remove any previously-created files.
         clean: {
             tests: ['tmp']
@@ -140,16 +130,7 @@ module.exports = function(grunt) {
     // Actually load this plugin's task(s).
     grunt.loadTasks('tasks');
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'atomizer', 'nodeunit']);
-
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['jshint', 'test']);
-
+    grunt.registerTask('test', ['atomizer', 'nodeunit', 'clean']);
 };
