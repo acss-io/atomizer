21bc6bf32f7e49b9396c71eb1cfadd312beab0d1
Seth Bertalotto <seth@bertalotto.net>
2022-08-04 15:13:34 -0700

    refactor(dev): remove grunt usage (#468)

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const baseConfig = {
+    entry: {
+        main: './app/client-reference.js',
+        repl: './app/assets/js/repl.js',
+        search: './app/assets/js/search.js',
+    },
+    module: {
+        rules: [{ test: /\.jsx?$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }],
+    },
+    output: {
+        path: path.resolve(__dirname, 'docs', 'assets', 'js'),
+        publicPath: '/public/js/',
+        filename: '[name].js',
+        chunkFilename: '[name].js',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+};
+
+module.exports = [
+    {
+        ...baseConfig,
+        devtool: 'source-map',
+        mode: 'development',
+        name: 'dev',
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify('development'),
+                },
+            }),
+        ],
+        stats: {
+            colors: true,
+        },
+        watch: true,
+    },
+    {
+        ...baseConfig,
+        mode: 'production',
+        name: 'prod',
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify('production'),
+                },
+            }),
+        ],
+        stats: 'errors-only',
+    }
+];
