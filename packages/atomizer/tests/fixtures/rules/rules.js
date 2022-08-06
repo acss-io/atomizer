b14a94b221862e281905dfc11bfa6af275b1089a
redonkulus <seth@bertalotto.net>
2022-08-05 23:32:52 -0700

    test: add binary tests

---
+++ packages/atomizer/tests/fixtures/rules/rules.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        allowParamToValue: true,
+        id: 'foo',
+        matcher: 'Foo',
+        name: 'Foo',
+        styles: {
+            'foo': '$0'
+        },
+        type: 'pattern'
+    }
+];
