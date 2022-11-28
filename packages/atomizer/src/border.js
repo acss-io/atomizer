f7425334a846ac0fcb05bd48aa7b10a8194385ec
Seth Bertalotto <seth@bertalotto.net>
2022-11-28 13:32:36 -0800

    feat: support outline-color, outline-style, outline-width (#539)

---
+++ packages/atomizer/src/border.js
@@ -0,0 +1,19 @@
+module.exports.borderStyles = {
+    a: 'auto',
+    d: 'dotted',
+    da: 'dashed',
+    do: 'double',
+    g: 'groove',
+    h: 'hidden',
+    i: 'inset',
+    n: 'none',
+    o: 'outset',
+    r: 'ridge',
+    s: 'solid',
+};
+
+module.exports.borderWidths = {
+    m: 'medium',
+    t: 'thin',
+    th: 'thick',
+};
