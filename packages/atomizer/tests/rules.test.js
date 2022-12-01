bd093aaa50c8d40b9f1837711188bad155ce0f5f
redonkulus <seth@bertalotto.net>
2022-11-30 16:11:51 -0800

    style: better comments

--- packages/atomizer/tests/rules.test.js
+++ packages/atomizer/tests/rules.test.js
@@ -15,14 +15,15 @@ describe('Rules', () => {
             // parse optional arguments; e.g: [ { k: v }, { k: v }, ... ]
             if (args) {
                 const numberOfArguments = args.length;
-                // parse classes for the first argument object
+                // create classes for the first argument object
                 const firstArgumentProps = Object.keys(args[0]);
                 for (let x = 0; x < firstArgumentProps.length; x++) {
                     classNames.push(`${matcher}(${firstArgumentProps[x]})`);
                 }
 
-                // parse classes for the second argument object
+                // create classes for the second argument object
                 // we just take the first argument and append each property from second argument object
+                // NOTE: some combinations of arguments are not valid when used on a page, but we don't check for that here
                 if (numberOfArguments > 1) {
                     const secondArgumentProps = Object.keys(args[1]);
                     for (let x = 0; x < secondArgumentProps.length; x++) {
@@ -31,22 +32,22 @@ describe('Rules', () => {
                 }
             }
 
-            // if this flag is set, then add an arbitrary unit to the class name
+            // if `allowParamToValue` is set, then add an arbitrary unit to the class name
             if (allowParamToValue) {
                 classNames.push(`${matcher}(1px)`);
             }
 
-            // handle aspect-ratio use case
+            // handle `calculatePercentage` use case
             if (calculatePercentage === false) {
                 classNames.push(`${matcher}(1/1)`);
             }
 
-            // handles shorthand classes with no arguments or "param to value" (e.g. Bdy)
+            // handles shorthand classes with no arguments or `allowParamToValue` (e.g. Bdy)
             if (!classNames.length) {
                 classNames.push(matcher);
             }
 
-            // replace style name if __START__ or __END__ exists
+            // replace style name if `__START__` or `__END__` exists
             const styleName = styleNames[s].replace('__START__', 'left').replace('__END__', 'right');
 
             it(styleName, () => {
